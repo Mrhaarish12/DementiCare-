af4acd799e79bfc169abbda2cb865b67ca8ff057
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2023-01-31 11:50:55 +0530

    done

--- backend1/settings.py
+++ backend1/settings.py
@@ -53,8 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'backend1.urls'
 
 TEMPLATES = [
