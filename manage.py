ff4316ca88db96efe67ee6f4b244af93ff86c352
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2023-01-31 14:45:27 +0530

    ok

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import dotenv
 
 def main():
     # dotenv.read_dotenv()
-    """Run administrative tasks."""
+    # """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend1.settings')
     try:
         from django.core.management import execute_from_command_line
